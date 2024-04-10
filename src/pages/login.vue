88a147c8251341344ddc8d4f5ad6af9a1168f90a
Mauricio Santos <mauwebjc@gmail.com>
2024-04-09 21:55:04 -0300

    (feature): inclusao de estrutura basica do formulario de login

---
+++ src/pages/login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <img src="./../assets/logo-fiap.svg" class="logo fiap" alt="FIAP logo" />
+        <form action="#">
+            <input type="text">
+            <input type="password" name="" id="">
+            <button type="button">LOGIN</button>
+        </form>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
