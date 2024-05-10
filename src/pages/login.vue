26e1d928b4659dc72e4c6f2cbc08659010dbe116
Laura <laura_mbarbosa@outlook.com>
2024-05-09 23:43:45 -0300

    adding login page

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,18 +1,33 @@
 <template>
     <div>
-        <img src="./../assets/logo-fiap.svg" class="logo fiap" alt="FIAP logo" />
-        <form action="#">
-            <input type="text">
-            <input type="password" name="" id="">
-            <button type="button">LOGIN</button>
-        </form>
+        <div id="app">
+            <form action="#">
+                <img src="./../assets/logo-fiap.svg" class="logo fiap" alt="FIAP logo" />
+                <div class ="fields">
+                    <div class ="field">
+                        <i class="pi pi-envelope" style="color: hsl(334, 82%, 51%);"></i>
+                        <input type="text" class="input" placeholder="Email">
+                    </div>
+                    <div class ="field">
+                        <i class="pi pi-lock" style="color: hsl(334, 82%, 51%);"></i>
+                    <input type="password" name="" class="input" placeholder="Senha">
+                    </div>
+                    <div class="form-footer">
+                        <a href="#" class="reset" ></a>
+                        <button type="button">Login</button>
+                    </div>
+                </div>   
+            </form>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-
+    import 'primeicons/primeicons.css'
 </script>
 
 <style scoped>
-
+button{
+    background-color: pink;
+}
 </style>
